c68fd2f7e568b9a2f228b8036f61b484ec57fa2e
Murio1305 <murilo.cage.rosa@escola.pr.gov.br>
2025-06-10 20:19:53 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Brasil',
+    'Quem foi Dom Pedro I e qual foi seu papel na independência do Brasil?',
+    'Dom Pedro I foi o primeiro imperador do Brasil. Ele proclamou a independência do país em 7 de setembro de 1822, rompendo os laços coloniais com Portugal e iniciando o Império do Brasil.'
+)
+
+criaCartao(
+    'Descobrimento',
+    'Quem descobriu o Brasil?',
+    'Pedro Álvares Cabral.em 1500'
+)
+
+criaCartao(
+    'Escravidão',
+    'O que foi a escravidão?',
+    'Um sistema onde pessoas eram forçadas a trabalhar sem liberdade ou salário.'
+)
+
+criaCartao(
+    'Tiradentes',
+    'Quem foi Tiradentes?',
+    'Um dos líderes da Inconfidência Mineira, morto em 1792.'
+)
